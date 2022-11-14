6b5d31343097ad4cd3f272426f36688c677f50f1
semih.ozkan <ozkansem@hotmail.com>
2022-11-15 01:41:37 +0300

    Add registration page

--- Infopackapp/urls.py
+++ Infopackapp/urls.py
@@ -1,21 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('Infopack.urls'))
-]
-
 from django.urls import path
 from . import views
 
-app_name = "main"   
-
-
 urlpatterns = [
-    path("", views.homepage, name="homepage"),
-    ...
-    path("register", views.register_request, name="register")
+    path('', views.registration, name='registration'),
 ]
