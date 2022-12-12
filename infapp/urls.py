c5d66ac7fd8b32c59aa4173c64cbc89672164fb6
semih.ozkan <ozkansem@hotmail.com>
2022-12-13 01:11:50 +0300

    fix mysql prolem

--- infapp/urls.py
+++ infapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -7,4 +8,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('saveuser/', views.saveuser, name='saveuser'),
     path('posts/', views.posts_list, name='posts_list'),
+    path('admin/', admin.site.urls),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+
 ]
