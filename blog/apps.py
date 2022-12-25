869e7724bbf14a8483e2aa38e9a69efe6cf7b1aa
sozkan4 <ozkansem@ahotmail.com>
2022-12-25 23:02:38 +0300

    change blog app config

--- blog/apps.py
+++ blog/apps.py
@@ -1,6 +1,4 @@
 from django.apps import AppConfig
 
-
-class InfappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'infapp'
+class BlogConfig(AppConfig):
+    name = 'blog'
