b7498dcfb8d4866fa426e5f6813579e637419931
sozkan4 <ozkansem@ahotmail.com>
2022-12-25 23:04:15 +0300

    add comment and post model

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from django.db.models.fields.related import ForeignKey
-from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 from ckeditor.fields import RichTextField
 
-# Create your models here.
+""" Post model """
 class Post(models.Model):
     title = models.CharField(max_length=150)
     content = RichTextField(blank=True, null=True)
@@ -29,6 +27,7 @@ class Post(models.Model):
         return reverse('post-detail', kwargs={"pk":self.pk})
 
 
+""" Comment model """
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name="comments" , on_delete=models.CASCADE)
     name = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -46,74 +45,3 @@ class Comment(models.Model):
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={"pk":self.pk})
 
-class FriendList(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user')
-    friends = models.ManyToManyField(User, blank=True, related_name='friends')
-    
-    def __str__(self):
-        return self.user.username
-
-    def add_friend(self, account):
-        if not account in self.friends.all():
-            self.friends.add(account)
-            self.save()
-
-    def remove_friend(self, account):
-        if account in self.friends.all():
-            self.friends.remove(account)
-            self.save()
-
-    def unfriend(self, removee):
-        remover_friends_list = self
-        remover_friends_list.remove_friend(removee)
-
-        friends_list = FriendList.objects.get(user=removee)
-        friends_list.remove_friend(self.user)
-
-    def is_mutual_friend(self, friend):
-        if friend in self.friends.all():
-            return True
-        return False
-
-class FriendRequest(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
-    is_active = models.BooleanField(blank=True, null=True, default=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.sender.username
-
-    def accept(self):
-        # update both sender and receiver friend list
-        receiver_friend_list = FriendList.objects.get(user=self.receiver)
-        if receiver_friend_list:
-            receiver_friend_list.add_friend(self.sender)
-            sender_friend_list = FriendList.objects.get(user=self.sender)
-            if sender_friend_list:
-                sender_friend_list.add_friend(self.receiver)
-                self.is_active = False
-                self.save()
-
-    def decline(self):
-        self.is_active = False
-        self.save()
-
-    def cancel(self):
-        self.is_active = False
-        self.save()
-
-
-class Notification(models.Model):
-    NOTIFICATION_TYPES = ((1,'Like'),(2,'Follow'),(3,'Comment'),(4,'Reply'),(5,'Like-Comment'),(6,'Like-Reply'))
-
-    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='notify_post', blank=True, null=True)
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notify_from_user')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notify_to_user')
-    notification_type = models.IntegerField(choices=NOTIFICATION_TYPES)
-    text_preview = models.CharField(max_length=120, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
-    is_seen = models.BooleanField(default=False)
-
-    def __str__(self):
-        return '%s - %s - %s - %s - %s' %(self.id, self.post, self.sender, self.user, self.notification_type)
