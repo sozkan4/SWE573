df03fc18fb19bc3e88d34bc5ec283622f6e642f7
sozkan4 <ozkansem@ahotmail.com>
2022-12-25 23:12:46 +0300

    change url configuration

--- Infopack/urls.py
+++ Infopack/urls.py
@@ -1,49 +1,34 @@
-"""infopack URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from django.urls import path, include
-from infapp import views as user_views
-from django.conf import settings
-from django.conf.urls.static import static
-
-
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('post/<post_title>', views.view_post, name='view_post'),
+    path('post/<post_title>/like', views.like_post, name='like_post'),
+    path('post/<str:post_title>/comment',  views.add_comment, name='add_comment'),
+    path('profile/<str:user>', views.view_profile, name='profile'),
+    path('profile/<str:user>/settings', views.settings, name='settings'),
 
-    path('register/', user_views.register, name='register'),
-    path('profile/', user_views.profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
-    
-    path('accounts/', include('allauth.urls')),
-    
-    path('', include('blog.urls')),
-    path('user/', include('users.urls')),
-    path('notifications/', include('notification.urls')),
-    path('chats/', include('chat.urls')),
-    path('vc/', include('videocall.urls')),
-    path('friend/', include('friend.urls', namespace='friend')),
+
+
+    path('search/', views.search, name='search'),
+    path('update_profile/<current_user>', views.update_profile, name='update_profile'),
+    path('change_image/<user>', views.change_image, name='change_image'),
+    path('delete_user/<user>', views.delete_user, name='delete_user'),
+
+
+
+    path('write_post/', views.write_post, name='write_post'),
+    path('post_created/', views.post_created, name='post_created'),
+    path('delete/<post_title>', views.delete_post, name='delete_post'),
+
+
+
+
+
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
+    path('contact/', views.contact, name='contact'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
