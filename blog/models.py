260bb8d9a0868745adcd2f1cb822600493f7e7d9
sozkan4 <ozkansem@ahotmail.com>
2022-12-25 23:10:07 +0300

    change model structure

--- blog/models.py
+++ blog/models.py
@@ -1,47 +1,43 @@
 from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import User
-from django.urls import reverse
-from ckeditor.fields import RichTextField
+from datetime import datetime
+# Create your models here.
 
-""" Post model """
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    bio = models.TextField(default="none")
+    profile_pic = models.ImageField(upload_to='pictures', default='default.jpg')
+    def __str__(self):
+        return self.first_name
+
+
+
 class Post(models.Model):
-    title = models.CharField(max_length=150)
-    content = RichTextField(blank=True, null=True)
-    date_posted = models.DateTimeField(default=timezone.now)
-    date_updated = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User, related_name="blogpost", blank=True)
-    saves = models.ManyToManyField(User, related_name="blogsave", blank=True)
-
-    def total_likes(self):
-        return self.likes.count()
-
-    def total_saves(self):
-        return self.saves.count()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    creation_date = models.DateField()
+    likes = models.ManyToManyField(User, related_name='likes')
 
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={"pk":self.pk})
 
-
-""" Comment model """
 class Comment(models.Model):
-    post = models.ForeignKey(Post, related_name="comments" , on_delete=models.CASCADE)
-    name = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField(max_length=200)
-    date_added = models.DateTimeField(auto_now_add=True)
-    likes = models.ManyToManyField(User, related_name="blogcomment", blank=True)
-    reply = models.ForeignKey('self', null=True, related_name="replies", on_delete=models.CASCADE)
-
-    def total_clikes(self):
-        return self.likes.count()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    name = models.CharField(default='no name',max_length=50)
+    comment = models.CharField(max_length=400)
 
     def __str__(self):
-        return '%s - %s - %s' %(self.post.title, self.name, self.id)
+        return self.name+"=>"+self.comment
 
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={"pk":self.pk})
 
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(default=None)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
