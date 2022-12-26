fa7ea66de63dac38234415fc4bd4f19177a46807
sozkan4 <ozkansem@ahotmail.com>
2022-12-27 02:29:45 +0300

    fix deployment error

--- Infopack/wsgi.py
+++ Infopack/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infopack.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Infopack.settings')
 
 application = get_wsgi_application()
