2eea46b4d96ca7e7eb0b24f224fb7f3e1cc39b4a
sozkan4 <ozkansem@ahotmail.com>
2022-12-27 03:28:22 +0300

    fix deploy

--- Infopack/asgi.py
+++ Infopack/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for infopack project.
+ASGI config for Infopack project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infopack.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Infopack.settings')
 
 application = get_asgi_application()
